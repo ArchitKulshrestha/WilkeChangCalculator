39d0736680e2fb37ec60c427b126650a82350439
Archit <archit499kul@gmail.com>
2024-02-11 11:19:41 +0530

    Update project dependencies and modify layout metadata

--- app/page.js
+++ app/page.js
@@ -1,113 +1,147 @@
-import Image from "next/image";
+"use client";
+import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [data, setData] = useState({});
+  const { register, handleSubmit } = useForm();
+  const [getDeffusivity, setGetDeffusivity] = useState(null);
+  const diffusivity = [];
+
+  useEffect(() => {
+    const calculateDeffusivity = () => {
+      if (
+        data.temperature &&
+        data.solvent &&
+        data.solventViscosity &&
+        data.soluteMolalVolume &&
+        data.solventMolarMass
+      ) {
+        const getDeffusivity = (T, phi, Mb, ub, Vca) => {
+          const Vta = 0.285 * Vca ** 1.048;
+          return (
+            (117.3 * Math.pow(10, -18) * Math.pow(phi * Mb, 0.5) * T) /
+            (ub * Math.pow(Vta, 0.6))
+          );
+        };
+
+        setGetDeffusivity((prevDeffusivity) =>
+          getDeffusivity(
+            data.temperature,
+            data.solvent,
+            data.solventMolarMass,
+            data.solventViscosity,
+            data.soluteMolalVolume
+          ).toExponential(4)
+        );
+      }
+    };
+
+    calculateDeffusivity();
+  }, [data]);
+
+  const onSubmit = (data) => {
+    if (data.solvent == data.soluteMolalVolume) {
+      alert("Association Coefficient and soluteMolalVolume cannot be same");
+      return;
+    }
+
+    setData(data);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="bg-gray-100 min-h-screen flex items-center justify-center px-5">
+      <form
+        className="w-full sm:w-[50%] mx-auto mt-8"
+        onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <label
+            htmlFor="temperature"
+            className="block text-gray-700 text-sm font-bold mb-2">
+            Temperature (kelvin)
+          </label>
+          <input
+            required
+            {...register("temperature")}
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="mb-4">
+          <label
+            htmlFor="solvent"
+            className="block text-gray-700 text-sm font-bold mb-2">
+            Association Coefficient(φ)
+          </label>
+          <select
+            required
+            {...register("solvent")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            defaultValue={2.26}>
+            <option value="2.26">Water</option>
+            <option value="1.5">Ethanol</option>
+            <option value="1.9">Methanol</option>
+            <option value="1">Benzene</option>
+            <option value="1">Hexane</option>
+            <option value="1">Toluene</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="solventViscosity"
+            className="block text-gray-700 text-sm font-bold mb-2">
+            solventViscosity (Pa.s)
+          </label>
+          <input
+            required
+            {...register("solventViscosity")}
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="soluteMolalVolume"
+            className="block text-gray-700 text-sm font-bold mb-2">
+            soluteMolalVolume (m3/Kmol)
+          </label>
+          <select
+            required
+            {...register("soluteMolalVolume")}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            defaultValue={1.5}>
+            <option value="2.26">Water</option>
+            <option value="1.5">Ethanol</option>
+            <option value="1.9">Methanol</option>
+            <option value="1">Benzene</option>
+            <option value="1">Hexane</option>
+            <option value="1">Toluene</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="solventMolarMass"
+            className="block text-gray-700 text-sm font-bold mb-2">
+            solventMolarMass (g/mol)
+          </label>
+          <input
+            required
+            {...register("solventMolarMass")}
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Submit
+          </button>
+        </div>
+        <div className="mb-4 text-gray-800 font-bold">
+          <p>Deffusivity: {getDeffusivity} m2/s</p>
+        </div>
+      </form>
+    </section>
   );
 }
